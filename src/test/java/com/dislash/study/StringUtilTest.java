db35c03bec2bc400295b005a25bc14462caeec58
kynam <kynam@dislash.com>
2016-05-05 12:29:17 +0900

    gitTestProject

---
+++ src/test/java/com/dislash/study/StringUtilTest.java
@@ -0,0 +1,13 @@
+package com.dislash.study;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringUtilTest {
+
+    @Test
+    public void testCheckEvenNumber() {
+        assertTrue(StringUtil.checkEvenNumber(2));
+    }
+}
